26aa3007bedb66e3f41c899d54071115587f24a8
gest2356 <vojta.javurek@gmail.com>
2026-03-23 12:43:50 +0100

    added: basic server endpoint and conenction

---
+++ server/server.mjs
@@ -0,0 +1,18 @@
+﻿import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(express.json());
+app.use(cors())
+
+const port = 3000
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+})
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the students book API :)');
+})
+
+
